1aa93780ba65695b9bc72bac95e850025556a594
widii11 <wdya1102@gmail.com>
2025-10-27 23:38:04 +0700

    Add next.config.js for static export

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: "/pomofocus_clone_055",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
